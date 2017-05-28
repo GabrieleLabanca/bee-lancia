78788e592f2386fc69db3ae242f7146f373d01a0
gabriele <gabrielelabanca@gmail.com>
2017-05-28 20:26:08 +0200

    plot_mean() working

--- dati/machine.cxx
+++ dati/machine.cxx
@@ -8,13 +8,25 @@ class datafile
   public:
     datafile(string,float);
 
+    void fill_mean(int);
+
+    int get_n_lines() { return lines; } 
+    int get_n_elab() { return n_elab; }
+
+
     void plot_data();
+    void plot_elab();
 
   private:
     const char * filename;
     int headlines, lines;
+    // actual data
     float * x;
     float * y;
+    // elaboration
+    int n_elab;
+    float * x_elab;
+    float * y_elab;
 
     void count_lines();
     void fill_data(float);
@@ -69,11 +81,58 @@ void datafile::plot_data()
   plot -> Draw();
 }
 
+void datafile::plot_elab()
+{
+  TGraph * plot = new TGraph (n_elab, x_elab, y_elab);	
+  plot -> Draw();
+}
+
+
+void datafile::fill_mean(int N)
+{
+  using namespace std;
+  int npoints = float(lines/N);
+  n_elab = npoints;
+//  cerr << "npoints " << npoints << endl;
+  x_elab = new float[npoints];
+  y_elab = new float[npoints];
+  float tsum = 0;
+  int ireal = 0; //counts from 0 to lines
+  for(int index=0; index<npoints; index++){ //counts the points in _elab
+    for(int i=0; i<N; i++){ //counts the points over which mean is taken
+    //  cerr << "y[i] " << y[ireal] << endl;
+      tsum += y[ireal];
+      ++ireal;
+    }
+   // cerr << "TSUM " << tsum << endl;
+    x_elab[index] = N*(index+1./2);
+    y_elab[index] = tsum/int(N);
+    //cerr << x_elab[index] << " " << y_elab[index] << endl;
+    tsum = 0;
+  }
+}
+
+
+
+
+
+
+
+
 void plot_file(string NAME, float UNIT=1)
 {
   datafile mydata(NAME,UNIT);
   mydata.plot_data();
 }
+
+void plot_mean(string NAME, float UNIT=1, int NMEAN=5)
+{
+  datafile mydata(NAME,UNIT);
+  mydata.fill_mean(NMEAN);
+  mydata.plot_elab();
+}
+
+
 
 
 
@@ -89,54 +148,54 @@ void plot_file(string NAME, float UNIT=1)
 
 //OLD plot.cxx
 /*
-int * n_lines(const char * name) { 
-  int number_of_header = 0;
-  int number_of_lines = -1;
-  std::string line;
-  std::ifstream myfile(name);
+   int * n_lines(const char * name) { 
+   int number_of_header = 0;
+   int number_of_lines = -1;
+   std::string line;
+   std::ifstream myfile(name);
 
-  while (std::getline(myfile, line))
-    if(line[0]!='#')
-      ++number_of_lines;
-    else if(line[0]=='#')
-      ++number_of_header;
-  int * numbers = new int[2];
-  numbers[0] = number_of_header;
-  numbers[1] = number_of_lines;
-  return numbers;
+   while (std::getline(myfile, line))
+   if(line[0]!='#')
+   ++number_of_lines;
+   else if(line[0]=='#')
+   ++number_of_header;
+   int * numbers = new int[2];
+   numbers[0] = number_of_header;
+   numbers[1] = number_of_lines;
+   return numbers;
+   }
+
+   float** plot(string filestring, float unit=1){
+
+   const char * filename = filestring.c_str();
+   int header = n_lines(filename)[0];
+   int lines  = n_lines(filename)[1];
+   std::cerr << header << "  " << lines << endl;
+
+   std::fstream file (filename, std::fstream::in);
+
+   std::string temp;
+   float * x = new float [lines];
+   float * y = new float [lines];
+
+   for (int i=0; i< header; i++){
+   std::getline(file, temp);
+//std::cerr << temp << endl;
+}
+for (int i =0; i<lines; i++){
+x[i] = i*unit; // rescale x
+file >> y[i];
+std::cerr << x[i] << "   " << y[i] << endl;
 }
 
-float** plot(string filestring, float unit=1){
+TGraph * plot = new TGraph (lines, x, y);	
+plot -> Draw();
 
-  const char * filename = filestring.c_str();
-  int header = n_lines(filename)[0];
-  int lines  = n_lines(filename)[1];
-  std::cerr << header << "  " << lines << endl;
+float** data_vect = new float*[2];
+data_vect[0] = x;
+data_vect[1] = y;
 
-  std::fstream file (filename, std::fstream::in);
-
-  std::string temp;
-  float * x = new float [lines];
-  float * y = new float [lines];
-
-  for (int i=0; i< header; i++){
-    std::getline(file, temp);
-    //std::cerr << temp << endl;
-  }
-  for (int i =0; i<lines; i++){
-    x[i] = i*unit; // rescale x
-    file >> y[i];
-    std::cerr << x[i] << "   " << y[i] << endl;
-  }
-
-  TGraph * plot = new TGraph (lines, x, y);	
-  plot -> Draw();
-
-  float** data_vect = new float*[2];
-  data_vect[0] = x;
-  data_vect[1] = y;
-
-  return data_vect;
+return data_vect;
 
 }
 
