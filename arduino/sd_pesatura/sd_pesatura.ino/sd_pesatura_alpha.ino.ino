798e7952801c9fec3fe5c879bc36c4f9b896769a
GabrieleLabanca <gabrielelabanca@gmail.com>
2017-09-08 19:47:42 +0200

    +sd working

--- arduino/sd_pesatura/sd_pesatura.ino/sd_pesatura_alpha.ino.ino
+++ arduino/sd_pesatura/sd_pesatura.ino/sd_pesatura_alpha.ino.ino
@@ -1,41 +1,33 @@
 #include <SPI.h>
 #include <SD.h> //to use sd card  
+#include "HX711.h"//The library used for arduino  https://github.com/bogde/HX711
 
-//The library used for arduino  https://github.com/bogde/HX711
-// LCD can also be used instead of serial 
-#include "HX711.h"
-// HX711.DOUT  - pin 10
-// HX711.PD_SCK - pin 11
-HX711 scale(10, 11); // parameter "gain" is ommited; the default value 128 is used by the lib
+/////////////////////////////////
+// setup HX711
+// HX711.DOUT  -  8 (BEFORE: pin 10)
+// HX711.PD_SCK - 7 (BEFORE: pin 11)
+//scale(DOUT,SCK)
+HX711 scale(7,6); // parameter "gain" is omitted; the default value 128 is used by the lib
+  
   
 File outfile;
 File myFile;
-  
+
 void setup() 
 {
-  scale.power_up();
-  delay(1000);;
   Serial.begin(38400);
-  //while (!Serial) {
-    //; // wait for serial port to connect. Needed for native USB port only
-  //}
+  Serial.println("BEGIN SERIAL");
 
-
-  Serial.println("HX711 Demo");  Serial.println("Before setting up the scale:");
+  /////////////////////////////////////
+  // set HX711 parameters
+  scale.power_up();
+  delay(1000);
+  
+  Serial.println("Before setting up the scale:");
   Serial.print("read: \t\t ");
   Serial.println(scale.read());     // print a raw reading from the ADC 
-  Serial.print("read average:\t\t ");
-  Serial.println(scale.read_average(20));       // print the average of 20 readings from the ADC
-  Serial.print("get value: \t\t ");
-  Serial.println(scale.get_value(5));   
-  // print the average of 5 readings from the ADC minus the tare weight (not set yet)
-  Serial.print("get units: \t\t ");
-  Serial.println(scale.get_units(5), 1);  
-  /// print the average of 5 readings from the ADC minus tare weight 
-  //(not set) divided by the SCALE parameter (not set yet) 
-  scale.set_scale(114.f);//scale.set_scale(2280.f);  
-  // this value is obtained by calibrating the scale with known weights; 
-  //see the README for details
+  float myscale = 114. / .005600966442953021;
+  scale.set_scale(myscale); // this value is obtained by calibrating the scale with known weights;
   scale.tare();   // reset the scale to 0
   Serial.println("After setting up the scale:");
   Serial.print("read: \t\t");
@@ -51,6 +43,8 @@ void setup()
   // print the average of 5 readings from the ADC minus tare weight, 
   //divided by the SCALE parameter set with set_scale
 
+
+  ////////////////////////////////////////
   // initialize SD card
   Serial.print("Initializing SD card...");
   if (!SD.begin(4)) {
@@ -59,25 +53,24 @@ void setup()
   }
   Serial.println("initialization done.");
 
-  if(SD.remove("data.txt")) Serial.println("Removed data.txt");
+  SD.remove("DATA.txt");
+  SD.remove("data.txt"); // problems with upper-/lowercase
+  
 
   Serial.println("Readings:");
 
-  
-
-
-
 }
+
 void loop() 
 {
   //Serial.print("Weight :");                        
   /* ---------Weight in terms of KG-------------*/
-  float measure = scale.get_units()* .005600966442953021;
+  float measure = scale.get_units();//* .005600966442953021;
   Serial.println(measure, 3); //tara con 3.870kg 
 
   outfile = SD.open("data.txt", FILE_WRITE);
   if(outfile){
-    outfile.println(measure);
+    outfile.println(measure,3);
     //outfile.print('\n');
     outfile.close();
   }
@@ -85,13 +78,10 @@ void loop()
     Serial.println("error opening data.txt");
   }
 
-
-
-  //Serial.print(scale.read());
-  //Serial.print("KG");
-
   //scale.power_down();             // put the ADC in sleep mode
   delay(1000);
-  //Serial.print("\n");  
   //scale.power_up();
+  
+
+  
 }
