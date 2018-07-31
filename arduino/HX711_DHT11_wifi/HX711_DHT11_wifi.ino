8262e5b20f2c8cb36b63db2d432266467cfe03e1
GabrieleLabanca <gabrielelabanca@gmail.com>
2018-07-31 09:59:15 +0200

    new approach: tare on ThingSpeak; latex modified consequently

--- arduino/HX711_DHT11_wifi/HX711_DHT11_wifi.ino
+++ arduino/HX711_DHT11_wifi/HX711_DHT11_wifi.ino
@@ -1,5 +1,12 @@
-// by Gabriele Labanca
-// credits: Robo India Tutorial
+///////////////////
+// CONFIGURE HX711
+#include "HX711.h"//The library used for arduino  https://github.com/bogde/HX711
+// HX711.DOUT   - D1  (Arduino: 8 (BEFORE: pin 10))
+// HX711.PD_SCK - D0 (Arduino: 7 (BEFORE: pin 11))
+#define HX711_SCK_PIN  D1
+#define HX711_DOUT_PIN D2
+//scale(DOUT,SCK)
+HX711 scale;//(HX711_DOUT_PIN, HX711_SCK_PIN); // parameter "gain" omitted; default value 128
 
 ///////////////////
 // CONFIGURE DHT11
@@ -9,71 +16,29 @@
 DHT dht(DHT11_PIN, DHTTYPE);
 //DHT dht(DHTPIN, DHT11);
 
-///////////////////
-// CONFIGURE HX711
-#include "HX711.h"//The library used for arduino  https://github.com/bogde/HX711
-// HX711.DOUT   - D2  (Arduino: 8 (BEFORE: pin 10))
-// HX711.PD_SCK - D1 (Arduino: 7 (BEFORE: pin 11))
-#define HX711_SCK_PIN  D1
-#define HX711_DOUT_PIN D2
-//scale(DOUT,SCK)
-HX711 scale;//(HX711_DOUT_PIN, HX711_SCK_PIN); // parameter "gain" omitted; default value 128
-
 // CONFIGURE WIFI
 #include <ESP8266WiFi.h>
-// CELL
-const char *ssid =  "gabgab";     // replace with your wifi ssid and wpa2 key
-const char *pass =  "12345678";
-/*// TELECOM
-const char *ssid =  "Telecom-58120441";     // replace with your wifi ssid and wpa2 key
-const char *pass =  "CviK6uXSQLucijkT1XT8BlFP";
-*/
-/*// FASTWEB
-const char *ssid =  "FASTWEB-1-0D2259";     // replace with your wifi ssid and wpa2 key
-const char *pass =  "CviK6uXSQLucijkT1XT8BlFP";
-*/
-//const char* server = "api.thingspeak.com";
-//String apiKey = "AG5BH0BV8ITOCAUL";     //  Enter your Write API key from ThingSpeak
-//WiFiClient client;
 
 
-//int step = 0;
+// WIFI CREDENTIALS
+const char *ssid = "FASTWEB-D68BC7";
+const char *pass = "CY3APAPE3J";
+
+// CONFIGURE SERVER
+const char* server = "api.thingspeak.com";
+String apiKey = "AG5BH0BV8ITOCAUL";     //  Enter your Write API key from ThingSpeak
+WiFiClient client;
 
 void setup()
 {
   delay(1000);
   Serial.begin(9600);
-  Serial.println("BEGIN CONNECTION");
-
-  delay(1000);
-  //dht.begin();
-
-  delay(1000);
-  //SETUP WIFI
-  Serial.println("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, pass);
-  int counter = 0;
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-    //if(++counter > 30) break;
-  }
-  Serial.println("");
-  /*if(WiFi.status() == WL_CONNECTED){
-    Serial.println("WiFi connected");
-  } else {
-    Serial.println("No WiFi");
-  }
-  */
- 
-
-/*
+  Serial.println("HX711_DHT11_wifi");
+  
   // SETUP HX711
-  scale.begin(D2,D1);
-  scale.power_up();
-  delay(1000);
+  scale.begin(HX711_DOUT_PIN, HX711_SCK_PIN);
+
+  /*delay(1000);
   Serial.println("Before setting up the scale:");
   Serial.print("read: \t\t ");
   Serial.println(scale.read());     // print a raw reading from the ADC
@@ -98,37 +63,45 @@ void setup()
   delay(15);
   // print the average of 5 readings from the ADC minus tare weight,
   //divided by the SCALE parameter set with set_scale
-*/
+  */
+
+  //SETUP WIFI
+  Serial.println("Connecting to ");
+  Serial.println(ssid);  Serial.println("HX711_DHT11_wifi");
+  WiFi.begin(ssid, pass);
+  while (WiFi.localIP().toString() == "0.0.0.0") //while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi connected");
+  //Serial.println("HX711_DHT11_wifi READY");
 
 }
-
 void loop()
 {
-  /*
+  // GET DATA  MWIFIRINALDO
   // weight
-  float weight = scale.get_units();//* .005600966442953021;
+  scale.power_up();
+  /*float weight = scale.get_units();//* .005600966442953021;
+  Serial.print("Weight: ");
+  Serial.print(weight, 3); //tara con 3.870kg
+  */
+  float weight_raw = scale.read();
+  Serial.print(" Raw weight: ");
+  Serial.print(weight_raw, 3);
+  scale.power_down(); 
   // temperature
   float t = dht.readTemperature();
   // humidity
   float h = dht.readHumidity();
-  if (isnan(weight)) {
-    Serial.println("ERROR READING from HX711!");
-    blink(10);
-    return;
-  }
-
-  blink(1);
-  Serial.print("Weight: ");
-  Serial.print(weight, 3); //tara con 3.870kg
-  Serial.print(' ');
-  Serial.print("Temperature: ");
+  Serial.print(" Temperature: ");
   Serial.print(t);
   Serial.print(" degrees Celsius, Humidity: ");
   Serial.print(h);
   Serial.print('\n');
-*/
 
-  /*
+  // SEND TO THINGSPEAK
   Serial.println("%. Send to Thingspeak.");
   if (isnan(t) || isnan(h))
   {
@@ -138,14 +111,15 @@ void loop()
   if (client.connect(server, 80))  //   "184.106.153.149" or api.thingspeak.com
   {
     String postStr = apiKey;
-    postStr += "&field1=";
-    postStr += String(weight);
+    //postStr += "&field1=";
+    //postStr += String(weight);
     postStr += "&field2=";
     postStr += String(t);
     postStr += "&field3=";
     postStr += String(h);
+    postStr += "&field4=";
+    postStr += String(weight_raw);
     postStr += "\r\n\r\n";
-
     client.print("POST /update HTTP/1.1\n");
     client.print("Host: api.thingspeak.com\n");
     client.print("Connection: close\n");
@@ -155,28 +129,20 @@ void loop()
     client.print(postStr.length());
     client.print("\n\n");
     client.print(postStr);
-
-
   }
   client.stop();
-  */
-/*
-  Serial.println("Waiting...");
 
-  // thingspeak needs minimum 15 sec delay between updates
-  delay(15000);
-  */
-  
+  // DELAY
+  Serial.print('\n');
+  int n_min = 1;
+  int delay_time = 1000 * 60 * n_min;
+  delay(delay_time);
 }
 
-/*
-void blink(int N){
-  for(int i=0; i<N; ++i){
-    digitalWrite(LED_BUILTIN, HIGH);   
-    delay(500);                   
-    digitalWrite(LED_BUILTIN, LOW);  
-    delay(1000);
-  }
-}
-*/
+
+
+
+
+
+
 
--- trash/make_graph.R
+++
@@ -1,9 +0,0 @@
-# R CMD BATCH "--args arg1 arg2" foo.R &
-voltage <- read.table(arg1)
-dimension <- dim(voltagee)
-time <- list(1:dimension[1])
-
-out_name <- paste(".", gsub("\\.dat",".png",arg1), sep="/")
-
-png(filename=out_name)
-plot(voltage,time)
